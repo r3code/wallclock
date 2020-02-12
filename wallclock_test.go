023aada9be24b9e0dfc6a8c6154c29e9cd48127e
r3code <contact@r3code.ru>
2020-02-12 17:32:18 +0300

    test: fixed test run, was too fast to compare timestamps

--- wallclock_test.go
+++ wallclock_test.go
@@ -9,21 +9,27 @@ import (
 
 func TestFakeMoment(t *testing.T) {
 	testMoment, _ := time.Parse(time.RFC3339, "1999-01-02T12:34:56.001Z")
-	var t1, t2, ftA time.Time
+	var t1, t2, t3, ftA time.Time
 	t1 = wallclock.Now()
+	time.Sleep(1 * time.Second)
 	cancelFunc := wallclock.FakeMoment(testMoment)
-	if !t1.After(testMoment) {
+	if t1.Equal(testMoment) {
 		t.Errorf("FakeMoment() failed to fake time")
 	}
-
 	ftA = wallclock.Now()
 	if !testMoment.Equal(ftA) {
 		t.Errorf("FakeMoment() failed, returned other time then set")
 	}
-	cancelFunc()
+	cancelFunc()	 
+	time.Sleep(1 * time.Second)
 	t2 = wallclock.Now()
 	if t2.Equal(testMoment) {
 		t.Errorf("FakeMoment() cancelFunc failed to return normal time")
+	} 
+	time.Sleep(1 * time.Second)
+	t3 = wallclock.Now()
+	if t3.Equal(t1) {
+		t.Errorf("FakeMoment() cancelFunc failed, t1=%v, t3=%v", t1.String(), t3.String())
 	}
 }
 
@@ -32,15 +38,19 @@ func TestFakeMomentDeferred(t *testing.T) {
 	var t1, t2, ftA, ftB time.Time
 	wrapDeferFunc := func() {
 		t1 = wallclock.Now()
-		defer wallclock.FakeMoment(testMoment)
+		time.Sleep(1 * time.Second)
+		cancelFunc := wallclock.FakeMoment(testMoment) 
+		defer cancelFunc()
 		ftA = wallclock.Now()
+		time.Sleep(1 * time.Second)
 		ftB = wallclock.Now()
 	}
 	wrapDeferFunc()
+	 
+	time.Sleep(1 * time.Second)
 	t2 = wallclock.Now()
-
 	if t1.Equal(t2) {
-		t.Errorf("FakeMoment() failed after defer called")
+		t.Errorf("FakeMoment() failed after defer called, t1=%v, t2=%v", t1.String(), t2.String())
 	}
 	if !ftA.Equal(ftB) {
 		t.Errorf("FakeMoment() fake times not equal %v != %v", ftA.String(), ftB.String())
@@ -51,16 +61,19 @@ func TestFakeMomentDeferred(t *testing.T) {
 func TestFakeFixedMoment(t *testing.T) {
 	var t1, t2, ftA, ftB time.Time
 	t1 = wallclock.Now()
+	time.Sleep(1 * time.Second)
 	cancelFunc := wallclock.FakeFixedMoment()
 	ftA = wallclock.Now()
+	time.Sleep(1 * time.Second)
 	ftB = wallclock.Now()
 	if t1.Equal(t2) {
-		t.Errorf("FakeFixedMoment() failed")
+		t.Errorf("FakeFixedMoment() failed, t1=%v, t2=%v", t1.String(), t2.String())
 	}
 	if !ftA.Equal(ftB) {
 		t.Errorf("FakeFixedMoment() fake times not equal %v != %v", ftA.String(), ftB.String())
 	}
-	cancelFunc()
+	cancelFunc()	
+	time.Sleep(1 * time.Second)
 	t2 = wallclock.Now()
 	if t2.Equal(ftA) {
 		t.Errorf("FakeFixedMoment() cancelFunc failed to return normal time")
@@ -71,15 +84,20 @@ func TestFakeFixedMomentDeferred(t *testing.T) {
 	var t1, t2, ftA, ftB time.Time
 	wrapDeferFunc := func() {
 		t1 = wallclock.Now()
-		defer wallclock.FakeFixedMoment()
+		time.Sleep(1 * time.Second)
+		cancelFunc := wallclock.FakeFixedMoment()
+		defer cancelFunc()
 		ftA = wallclock.Now()
+		time.Sleep(1 * time.Second)
 		ftB = wallclock.Now()
 	}
 	wrapDeferFunc()
+	
+	time.Sleep(1 * time.Second)
 	t2 = wallclock.Now()
 
 	if t1.Equal(t2) {
-		t.Errorf("FakeFixedMoment() failed after defer called")
+		t.Errorf("FakeFixedMoment() failed after defer called, t1=%v, t2=%v", t1.String(), t2.String())
 	}
 	if !ftA.Equal(ftB) {
 		t.Errorf("FakeFixedMoment() fake times not equal %v != %v", ftA.String(), ftB.String())
