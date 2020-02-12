3d7a67f62c997d09eddad12f3484597f3acd5b06
r3code <contact@r3code.ru>
2020-02-12 16:22:37 +0300

    refac: code refactored, functions renamed
    FakeTime became FakeFixedMoment

--- wallclock.go
+++ wallclock.go
@@ -6,33 +6,19 @@ var (
 	nowFunc = currentTimeUTC
 )
 
-func currentTimeUTC() time.Time {
-	return time.Now().UTC()
-}
-
 // Now gets tim by calling assigned nowFunc. 
 func Now() time.Time {
 	return nowFunc()
 }
 
-// FakeTime changes wallclock.Now behaviour to return "2014-11-12T11:45:26.371Z".
+// FakeMoment changes wallclock.Now behaviour to return the time specified by `timeMoment`.
 // Returns cancelFunc which should be called to turn time back to realtime.
 // Example:
-//   cancelFake = wallclock.FakeTime()
+//   cancelFake = wallclock.FakeMoment()
 //   // manual cancel
 //   cancelFake() // or defer cancelFake()
 // or
-//   defer wallclock.FakeTime()()
-func FakeTime() (cancelFunc func()) {
-	nowFunc = fakeNow
-	cancelFunc = func() {
-		nowFunc = currentTimeUTC
-	}
-	return cancelFunc
-}
-
-// FakeMoment changes wallclock.Now behaviour to return the time specified by `timeMoment`.
-// Returns cancelFunc which should be called to turn time back to realtime.
+//   defer wallclock.FakeMoment()()
 func FakeMoment(timeMoment time.Time) (cancelFunc func()) {
 	nowFunc = func() time.Time {
 		return timeMoment
@@ -43,7 +29,20 @@ func FakeMoment(timeMoment time.Time) (cancelFunc func()) {
 	return cancelFunc
 }
 
-func fakeNow() time.Time {
-	timeNow, _ := time.Parse(time.RFC3339, "2014-11-12T11:45:26.371Z")
-	return timeNow
+// FakeFixedMoment changes wallclock.Now behaviour to return "2014-11-12T11:45:26.371Z".
+// Returns cancelFunc which should be called to turn time back to realtime.
+func FakeFixedMoment() (cancelFunc func()) {
+  timeFrozen, _ := time.Parse(time.RFC3339, "2014-11-12T11:45:26.371Z") 
+	nowFunc = func() time.Time {	
+	  return timeFrozen
+  }
+	cancelFunc = func() {
+		nowFunc = currentTimeUTC
+	}
+	return cancelFunc
+}
+
+// return realtime clock 
+func currentTimeUTC() time.Time {
+	return time.Now().UTC()
 }
--- wallclock_test.go
+++ wallclock_test.go
@@ -7,44 +7,6 @@ import (
 	"github.com/r3code/wallclock"
 )
 
-func TestFakeTime(t *testing.T) {
-	var t1, t2, ftA, ftB time.Time
-	t1 = wallclock.Now()
-	cancelFunc := wallclock.FakeTime()
-	ftA = wallclock.Now()
-	ftB = wallclock.Now()
-	if t1.Equal(t2) {
-		t.Errorf("FakeTime() failed")
-	}
-	if !ftA.Equal(ftB) {
-		t.Errorf("FakeTime() fake times not equal %v != %v", ftA.String(), ftB.String())
-	}
-	cancelFunc()
-	t2 = wallclock.Now()
-	if t2.Equal(ftA) {
-		t.Errorf("FakeTime() cancelFunc failed to return normal time")
-	}
-}
-
-func TestFakeTimeDeferred(t *testing.T) {
-	var t1, t2, ftA, ftB time.Time
-	wrapDeferFunc := func() {
-		t1 = wallclock.Now()
-		defer wallclock.FakeTime()
-		ftA = wallclock.Now()
-		ftB = wallclock.Now()
-	}
-	wrapDeferFunc()
-	t2 = wallclock.Now()
-
-	if t1.Equal(t2) {
-		t.Errorf("FakeTime() failed after defer called")
-	}
-	if !ftA.Equal(ftB) {
-		t.Errorf("FakeTime() fake times not equal %v != %v", ftA.String(), ftB.String())
-	}
-}
-
 func TestFakeMoment(t *testing.T) {
 	testMoment, _ := time.Parse(time.RFC3339, "1999-01-02T12:34:56.001Z")
 	var t1, t2, ftA time.Time
@@ -82,5 +44,44 @@ func TestFakeMomentDeferred(t *testing.T) {
 	}
 	if !ftA.Equal(ftB) {
 		t.Errorf("FakeMoment() fake times not equal %v != %v", ftA.String(), ftB.String())
+	}
+}
+
+
+func TestFakeFixedMoment(t *testing.T) {
+	var t1, t2, ftA, ftB time.Time
+	t1 = wallclock.Now()
+	cancelFunc := wallclock.FakeFixedMoment()
+	ftA = wallclock.Now()
+	ftB = wallclock.Now()
+	if t1.Equal(t2) {
+		t.Errorf("FakeFixedMoment() failed")
+	}
+	if !ftA.Equal(ftB) {
+		t.Errorf("FakeFixedMoment() fake times not equal %v != %v", ftA.String(), ftB.String())
+	}
+	cancelFunc()
+	t2 = wallclock.Now()
+	if t2.Equal(ftA) {
+		t.Errorf("FakeFixedMoment() cancelFunc failed to return normal time")
+	}
+}
+
+func TestFakeFixedMomentDeferred(t *testing.T) {
+	var t1, t2, ftA, ftB time.Time
+	wrapDeferFunc := func() {
+		t1 = wallclock.Now()
+		defer wallclock.FakeFixedMoment()
+		ftA = wallclock.Now()
+		ftB = wallclock.Now()
+	}
+	wrapDeferFunc()
+	t2 = wallclock.Now()
+
+	if t1.Equal(t2) {
+		t.Errorf("FakeFixedMoment() failed after defer called")
+	}
+	if !ftA.Equal(ftB) {
+		t.Errorf("FakeFixedMoment() fake times not equal %v != %v", ftA.String(), ftB.String())
 	}
 }
